b0519fc0f58a2f50e271b9f0889e7af0d8c8f5c7
Guilherme Pombo <gpombotky@gmail.com>
2026-04-22 21:35:50 -0300

    implementa-classe-cachorro

--- testando.ts
+++ testando.ts
@@ -1,1 +1,23 @@
 console.log("Ola mundo!");
+
+class cachorro {
+  private nome: string;
+  private idade: number;
+
+  constructor() {
+    this.nome = "";
+    this.idade = 0;
+  }
+
+  definir(nome: string, idade: number) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  getNome(): string {
+    return this.nome;
+  }
+}
+
+
+const rex = new cachorro();
